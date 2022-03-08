4e2b7e7530d6b39e2d23b35e66ddf069198f8eee
bornb <bornb@192.168.219.103>
2022-03-08 12:36:46 +0900

    .

---
+++ src/com/apeach/lecture/function/Phone.java
@@ -0,0 +1,5 @@
+package com.apeach.lecture.function;
+
+public class Phone {
+
+}
---
+++ src/com/apeach/lecture/function/SmartPhoneExample.java
@@ -0,0 +1,12 @@
+//package com.apeach.lecture.function;
+//
+//public class SmartPhoneExample {
+//
+//	public static void main(String[] args) {
+////		Phone phone = new Phone();
+//		
+//		SmartPhone smartPhone = new Phone("asdf");
+//
+//	}
+//
+//}
